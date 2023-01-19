b58ca8c591d129e86a556478d054428103f69be1
Austin Klein <austinklein34@gmail.com>
2023-01-18 18:04:24 -0700

    adding tests

--- src/app/tasks/components/header/header.component.spec.ts
+++ src/app/tasks/components/header/header.component.spec.ts
@@ -23,4 +23,32 @@ describe('HeaderComponent', () => {
   it('text should be null', () => {
     expect(component.text).toEqual('');
   });
+  it('text should be what is typed into input', () => {
+    const input = fixture.debugElement.query(By.css('input'));
+    expect(input.nativeElement.value).toBe('');
+
+    // dispatch correct event
+    const event = new KeyboardEvent('keyup', {
+      bubbles: true,
+      cancelable: true,
+      shiftKey: false,
+    });
+
+    input.nativeElement.value = 'test';
+    input.nativeElement.dispatchEvent(event);
+    expect(component.text).toBe('test');
+  });
+  it('should call addTask function when enter is pressed', () => {
+    const input = fixture.debugElement.query(By.css('input'));
+    const addTaskSpy = spyOn(component, 'addTask');
+    // dispatch correct event
+    const event = new KeyboardEvent('keyup', {
+      bubbles: true,
+      cancelable: true,
+      shiftKey: false,
+      key: 'Enter',
+    });
+    input.nativeElement.dispatchEvent(event);
+    expect(addTaskSpy.calls.count()).toEqual(1);
+  });
 });
